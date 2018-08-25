0182bb48da447c311e2a9b88bb014db43ae19f53
sumedh3113 <sumedhdeshpande31@gmail.com>
2018-08-25 09:17:43 -0700

    updated python route file

--- Flask-Project/flask_blog/routes.py
+++ Flask-Project/flask_blog/routes.py
@@ -6,37 +6,27 @@ import secrets
 from PIL import Image
 # for resizing the image
 
-from flask import Flask,render_template,url_for,flash, redirect,request
-from flask_blog import app,db,bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask import Flask,render_template,url_for,flash, redirect,request, abort
+from flask_blog import app,db,bcrypt, mail
+from flask_blog.forms import (RegistrationForm, LoginForm, UpdateAccountForm, PostForm, 
+                             RequestResetForm, ResetPasswordForm)
 
 from flask_blog.models import User,Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-
-posts = [
-{
-	"author": "Sumedh D",
-	"title":"Post 1 ",
-	"content":"First port",
-	"date":"April 20 2018",
-
-},
-
-{
-	"author": "SD",
-	"title":"Post 2 ",
-	"content":"Second port",
-	"date":"April 25 2018"
-
-}
+from flask_mail import Message
 
 
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    # get page 1 by default and type is int so if someone passes value other than int it will throw an error 
+    page = request.args.get('page', 1 , type = int)
+    # to grab all the post from database
+    #posts = Post.query.all()
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page=2)
+    
     return render_template("home.html",postss=posts)
     #this postss variable will be accessed in html templates to get data from posts list
 
@@ -117,7 +107,7 @@ def save_picture(form_picture):
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
-    # i is new form_picture
+    # i is new form_picture 
     i.save(picture_path)
     
      
@@ -151,3 +141,122 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file, form=form)
+
+
+#for adding the post
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        # here we are creating the post
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    # legend is used to pass title of the form same in update post function
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
+
+
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    # fetching post if post is not exist return 404 error
+    post = Post.query.get_or_404(post_id)
+    # every post has a id and seperate html page will be assigned to it
+    return render_template('post.html', title=post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    # so that the user who wrote the post will be able to update it
+    if post.author != current_user:
+        # 403 is used for forbidden access
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        # to populate the update form with existing post data 
+        post.title = form.title.data
+        post.content = form.content.data
+        # we dont need to do add as these changes are already in the database we just need to update it
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=2)
+    return render_template('user_posts.html', posts=posts, user=user)
+
+# body msg of mail with reset link external is used to get external url not relative url 
+# relative url is used for internal link external for full seperate domain
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    # subject , sender & receiver of mail
+    msg = Message('Password Reset Request',
+                  sender='sumedhdeshpande31@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title='Reset Password', form=form)
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+        # this verify method is defined in models.py
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('reset_request'))
+    # this form variable will collect data from the ResetPasswordForm() field
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
